89a55a7396357a38ce5057f7431dbf92ff466016
Zachary NOGUEIRA <znogueir@e1r4p11.clusters.42paris.fr>
2023-04-07 22:23:19 +0200

    ambient lighting looks noice;

--- develop/srcs/graphic/ray_tracing.c
+++ develop/srcs/graphic/ray_tracing.c
@@ -52,14 +52,14 @@ void	reset_color_ratio(t_pixel *p)
 {
 	int	highest;
 
-	if (p->color.r == 0 && p->color.g == 0 && p->color.b == 0)
-		return ;
 	if (p->color.r >= p->color.g && p->color.r >= p->color.b)
 		highest = p->color.r;
 	else if (p->color.g >= p->color.b)
 		highest = p->color.g;
 	else
 		highest = p->color.b;
+	if (highest <= 0)
+		return ;
 	p->color.r = 255 * p->color.r / highest;
 	p->color.g = 255 * p->color.g / highest;
 	p->color.b = 255 * p->color.b / highest;
@@ -69,29 +69,46 @@ void	get_color(t_pixel pixel, t_pixel *p, t_data *d, double d_to_l)
 {
 	t_vector	v;
 	double		angle;
+	t_color		ret;
+	t_vector	color_ratio;
 
 	// (void)pixel;
 	// (void)d_to_l;
+	ret = (t_color){0, 0, 0};
 	v = normalize_vect(vec_sub(d->light->cord, vec_sum(d->cam->cord, \
 	vec_scale(p->pos, p->scaler))));
 	angle = dot_product(v, p->normal);
 	if (angle < 0)
 		angle = 0;
-	p->color.r *= (d->light->color.r * d->light->ratio / 255) * angle;
-	p->color.g *= (d->light->color.g * d->light->ratio / 255) * angle;
-	p->color.b *= (d->light->color.b * d->light->ratio / 255) * angle;
-	p->color.r += (d->ambient->color.r * d->ambient->ratio / 255) * pixel.color.r;
-	p->color.g += (d->ambient->color.g * d->ambient->ratio / 255) * pixel.color.g;
-	p->color.b += (d->ambient->color.b * d->ambient->ratio / 255) * pixel.color.b;
+	// printf("ambient : %d, %d, %d ; scaler : %f; d_to_l : %f;\n", \
+	// p->color.r, p->color.g, p->color.b, pixel.scaler, d_to_l);
+	// p->color.r = (d->ambient->color.r * d->ambient->ratio / 255) * p->color.r;
+	// p->color.g = (d->ambient->color.g * d->ambient->ratio / 255) * p->color.g;
+	// p->color.b = (d->ambient->color.b * d->ambient->ratio / 255) * p->color.b;
+	// printf("pixel: %d, %d. %d\n", p->color.r, p->color.g, p->color.b);
+	// printf("pixel.el: %d, %d. %d\n", pixel.color.r, pixel.color.g, pixel.color.b);
+	color_ratio.x = (double)p->color.r / 255;
+	color_ratio.y = (double)p->color.g / 255;
+	color_ratio.z = (double)p->color.b / 255;
+	ret.r = color_ratio.x * d->ambient->color.r * d->ambient->ratio;
+	ret.g = color_ratio.y * d->ambient->color.g * d->ambient->ratio;
+	ret.b = color_ratio.z * d->ambient->color.b * d->ambient->ratio;
+	if (pixel.scaler > d_to_l || pixel.scaler < 0)
+	{
+		ret.r += color_ratio.x * d->light->color.r * d->light->ratio * angle;
+		ret.g += color_ratio.y * d->light->color.g * d->light->ratio * angle;
+		ret.b += color_ratio.z * d->light->color.b * d->light->ratio * angle;
+	}
+	// p->color.r += (d->light->color.r * d->light->ratio / 255) * pixel.color.r;
+	// p->color.g += (d->light->color.g * d->light->ratio / 255) * pixel.color.g;
+	// p->color.b += (d->light->color.b * d->light->ratio / 255) * pixel.color.b;
+		// printf("diffuse : %d, %d, %d ; scaler : %f; d_to_l : %f;\n", \
+		// p->color.r, p->color.g, p->color.b, pixel.scaler, d_to_l);
+	p->color.r = ret.r;
+	p->color.g = ret.g;
+	p->color.b = ret.b;
 	if (p->color.r > 255 || p->color.g > 255 || p->color.b > 255)
 		reset_color_ratio(p);
-	// printf("r : %d; g : %d; b : %d;\n", p->color.r, p->color.g, p->color.b);
-	if (pixel.scaler < d_to_l && pixel.scaler > 0 && angle != 0)
-	{
-		p->color.r *= (d->ambient->color.r * d->ambient->ratio / 255);
-		p->color.g *= (d->ambient->color.g * d->ambient->ratio / 255);
-		p->color.b *= (d->ambient->color.b * d->ambient->ratio / 255);
-	}
 }
 
 void	trace_shadow(t_pixel *p, t_data *d, t_vector v, double d_to_l)
@@ -105,7 +122,7 @@ void	trace_shadow(t_pixel *p, t_data *d, t_vector v, double d_to_l)
 	// 	set_color(&p->color, "0,0,0");
 	i = 0;
 	ft_bzero(&pixel, sizeof(t_pixel));
-	set_color(&pixel.color, "1,1,1");
+	set_color(&pixel.color, "0,0,0");
 	pixel.scaler = -1;
 	d->cur_p.pos = v;
 	while (i < d->nbr_objs)
